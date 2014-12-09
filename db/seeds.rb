83b2c745cdce7e274b6b1eb3538ef886916fcd47
lalnuo <lalli.nuorteva@relex.fi>
2014-12-09 16:59:39 +0200

    seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+[User, Comment, Link, Section].each { |m| m.destroy_all }
+User.create!(:password =>"123456", :username => "fooguy")
+Comment.create!(:comment => "bad search engine", :score => -1, :user_id => 1, :link_id => 1)
+Link.create!(:link=>"http://www.google.fi", :score => 1, :section_id => 1, :user_id => 1)
+Section.create!(:name => "Main")
