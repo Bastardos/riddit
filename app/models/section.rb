f06ffa338afb13844f5460d1514a429ddd59e61b
lalnuo <lalli.nuorteva@relex.fi>
2014-12-09 17:42:59 +0200

    Validations for section

--- app/models/section.rb
+++ app/models/section.rb
@@ -1,5 +1,12 @@
 class Section < ActiveRecord::Base
   # Associations
   has_many(:links)
+
+  # Validations
+  validates(:name,
+      :uniqueness => {
+        :case_sensitive => false
+    }
+  )
 end
 
